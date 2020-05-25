60a0abaf31b04faf7126a4176132aec27c55a1aa
Yuu-bytes <osuperyuu@gmail.com>
2020-05-25 20:04:58 -0300

    criado o mvc - aula 25/05

---
+++ MVC_25-05-20/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_25_05_20.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
