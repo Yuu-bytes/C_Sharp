4bc563d8a2f45cc65473d6ac9bbefab62d8bece4
Yuu-bytes <osuperyuu@gmail.com>
2020-06-01 21:56:22 -0300

    Aula 01/06 sobre MVC

---
+++ Ecommerce/Controllers/ProductCrontoller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ecommerce.Models;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+    List<Product> lsProduct = new List<Product>();
+
+    public  ProductController(){
+        lsProduct.Add(new Product{
+            Id = 1,
+            Name = "Televisor"
+        });
+        lsProduct.Add(new Product{
+            Id = 2,
+            Name = "Smarphone"
+        });
+        lsProduct.Add(new Product{
+            Id = 3,
+            Name = "Notebook"
+        });
+    }
+        public IActionResult Index()
+        {
+            return View(lsProduct);
+        }
+    }
+}
---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
