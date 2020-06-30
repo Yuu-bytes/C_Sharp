6a2048e6dbc8d7e2a033f0fdb4c2aa470af3c234
Michael <osuperyuu@gmail.com>
2020-06-30 13:17:39 -0300

    Atividade que não funga

--- PressaoDiario/Data/DTOs/Pressao.cs
+++ PressaoDiario/Data/DTOs/Pressao.cs
@@ -9,7 +9,7 @@ namespace Data.DTOs
         public int Sistolica { get; set; }
         public int Diastolica { get; set; }
         public int Batimentos { get; set; }
-
         public int Periodo { get; set; }
+        public bool Visualizado { get; set; }
     }
 }
